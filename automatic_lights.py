76a24f7b7603ad6292630ad8b96dca802a565b49
Scott Witscher <scott@witscher.org>
2019-07-04 11:46:15 -0700

    Add automatic_lights.py script.
    Implement AstralTimer class which allows waiting
    until different astral events like dawn, sunrise,
    sunset and dusk.

---
+++ automatic_lights.py
@@ -0,0 +1,59 @@
+import time
+import datetime
+import logging
+import pytz
+import astral
+
+
+class AstralTimer:
+    def __init__(self, city_name):
+        ast = astral.Astral()
+        ast.solar_depression = 'civil'
+        self.city_name = city_name
+        self.city = ast[city_name]
+        self.city_timezone = pytz.timezone(self.city.timezone)
+        self.city_info()
+
+    def city_info(self):
+        logging.info('Information for {}/{}'.format(self.city_name, self.city.region))
+        logging.info('Timezone: {}'.format(self.city_timezone))
+        logging.info('Latitude: {:f}; Longitude: {:f}'.format(self.city.latitude, self.city.longitude))
+
+    def sun(self, date):
+        return
+
+    def sunset_info(self, sun):
+        logging.info('Sunset:  {}'.format(str(sun['sunset'])))
+        logging.info('Dusk:    {}'.format(str(sun['dusk'])))
+
+    def sunrise_info(self, sun):
+        logging.info('Tomorrow dawn: {}'.format(sun['dawn']))
+        logging.info('Tomorrow sunrise: {}'.format(sun['sunrise']))
+
+    def compute_duration(self, event_time):
+        now = datetime.datetime.now(tz=self.city_timezone)
+        duration = (event_time - now).total_seconds() / 2
+        return duration
+
+    def wait_until(self, event, date):
+        sun = self.city.sun(date=date, local=True)
+        event_time = sun[event]
+        logging.info("wait until {} on {} which is {}".format(event, date.date(), event_time))
+        duration = self.compute_duration(event_time,)
+        while duration > 10:
+            logging.info("sleeping for {}".format(duration))
+            time.sleep(duration)
+            duration = self.compute_duration(event_time)
+        logging.info("{} happening now".format(event))
+
+
+logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s', level=logging.INFO)
+
+astral_timer = AstralTimer('Salem')
+
+while True:
+    today = datetime.datetime.today()
+    astral_timer.wait_until('sunset', today)
+
+    tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+    astral_timer.wait_until('sunrise', tomorrow)
