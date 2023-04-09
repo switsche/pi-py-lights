c9f18b3618491ecb94b85e24431cecda3f3d9623
Scott Witscher <scott@witscher.org>
2023-04-08 19:44:42 -0700

    Update automatic_lights

--- automatic_lights.py
+++ automatic_lights.py
@@ -2,15 +2,20 @@ import logging
 import datetime
 import astral_timer
 
-logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format="%(levelname)s %(asctime)s: %(message)s",
+    level=logging.INFO,
+    filename="astral_lights.log",
+    encoding="utf-8",
+)
 
-at = astral_timer.AstralTimer('Salem')
+at = astral_timer.AstralTimer("Salem")
 
 day = datetime.datetime.today()
 
 while True:
-    at.wait_until('sunset', day)
+    at.wait_until("sunset", day)
 
-    at.wait_until('dusk', day)
+    at.wait_until("dusk", day)
 
     day += datetime.timedelta(days=1)
