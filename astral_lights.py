f5a37b75e49b53cc7fc7c0a87f745b4df75d2b72
Scott Witscher <scott@witscher.org>
2023-04-09 20:28:01 -0700

    Update astral_lights.py

--- astral_lights.py
+++ astral_lights.py
@@ -1,23 +1,36 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 
 import logging
 import datetime
 import astral_timer
+import RPi.GPIO as GPIO
 
 logging.basicConfig(
     format="%(levelname)s %(asctime)s: %(message)s",
     level=logging.INFO,
     filename="astral_lights.log",
-    encoding="utf-8",
 )
 
 at = astral_timer.AstralTimer("Salem")
-
 day = datetime.datetime.today()
+sun = at.city.sun(date=day, local=True)
+
+GPIO.setmode(GPIO.BCM)
+
+pins = [5]
+
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
 
 while True:
+    at.sunset_info(sun)
     at.wait_until("sunset", day)
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
 
     at.wait_until("dusk", day)
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
 
     day += datetime.timedelta(days=1)
